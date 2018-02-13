b3d4d2baa50d04c356a706f3713b068e521597b8
isminoula <lourent2@illinois.edu>
2018-02-13 16:33:42 -0600

    changing structure

--- chicago_crime_analysis.py
+++ chicago_crime_analysis.py
@@ -12,62 +12,68 @@ from os import listdir
 from os.path import isfile, join
 from os import walk
 from datetime import datetime
+from datetime import timedelta
+import logging
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
-def analyze(distance_range, time_range, target_info, data_file):
+def closest(target_file_row, data_file_row, time_range, distance_range):
+    try:
+        distance = geopy.distance.vincenty((target_file_row.lat, target_file_row.lon), (data_file_row.lat, data_file_row.lon)).m
+        delta = abs(data_file_row.time - target_file_row.time)
+        return delta <= timedelta(seconds=time_range) and distance <= distance_range
+    except:
+        return False
+
+def analyze(distance_range, time_range, target_file, data_file):
     satisfied_target = []
-    for target in target_info:
-        for csv_file in target_info[target]:
-            for user_index in range(len(csv_file)):
-                x_t = csv_file['lon'][user_index] #x
-                y_t = csv_file['lat'][user_index] #y
-                time_t = csv_file['time'][user_index] #time
-
-                if len(time_t) <= 20: continue #Invalid data
-
-                date_str_t = csv_file['time'][user_index][0:10]
-                time_str_t = csv_file['time'][user_index][11:19]
-                date_time_str_t = date_str_t + ' ' + time_str_t
-                date_time_t = datetime.strptime(date_time_str_t, "%Y-%m-%d %H:%M:%S")
-                target_location = (x_t, y_t)
-                for data_index in range(len(data_file)):
-                    x_d = data_file['X'][data_index] #x
-                    y_d = data_file['Y'][data_index] #y
-                    time_d = data_file['USER_Clrdate'][data_index] #time
-                    type_d = data_file['USER_Event_Type'][data_index] #type
-
-                    if isinstance(time_d, float) == True: continue #Invalid data
-
-                    date_time_d = datetime.strptime(time_d, "%m/%d/%Y %H:%M:%S")
-                    data_location = (x_d, y_d)
-
-                    time_diff = abs(date_time_t - date_time_d).total_seconds()
-                    loca_diff = geopy.distance.vincenty(target_location, data_location).m
-
-                    if loca_diff < distance_range and time_diff < time_range:
-                        info_list = [x_t, y_t, time_t, x_d, y_d, time_d, type_d]
-                        satisfied_target.append(info_list)
-
+    for target_index, target_row in target_file.iterrows():
+        logger.info('Checking target row {}'.format(target_index))
+        for data_index, data_row in data_file.iterrows():
+            if(data_index%10000==0):
+                logger.info('Checking data row {}'.format(data_index))
+            if(closest(target_row, data_row, time_range, distance_range)):
+                #time_t, y_t, x_t, x_d, y_d, time_t, type_d
+                info_list = target_row.tolist()
+                info_list.extend(data_row.tolist())
+                satisfied_target.append(info_list)
+        logger.info('Finished target row {}'.format(target_index))
     return satisfied_target
+
 
 if __name__ == "__main__":
     #Columns that are included in data_file
-    fields = ['X', 'Y', 'USER_Event_Type', 'USER_Clrdate']
-
+    fields = ['X', 'Y', 'USER_Event_Type', 'USER_Entry_Date___Time'] #changed the date field to the one that looks correct need to ask
+    folder = '/Users/ismini/Documents/Dataset/GPS/'
+    filename = 'FA171521_ARCGIS_GPS_50.csv'
     #Include the x, y, user_event_type, user_clrdate
-    data_file = pandas.read_csv('/Users/apple/Desktop/Dataset/GPS/FA171521_ARCGIS_GPS_50.csv', skipinitialspace=True, usecols=fields)
+    logger.info('Started reading data file')
+    data_file = pandas.read_csv(os.path.join(folder, filename),skipinitialspace=True, usecols=fields)
+    data_file = data_file.rename(columns={'USER_Entry_Date___Time': 'time', 'X':'lon', 'Y':'lat'}) #lets keep all times times
+    data_file = data_file[data_file.time.notnull()] # remove all NaN time values
+    data_file['time'] = pandas.to_datetime(data_file.time)
+    logger.info('Finished reading data file')
 
     #Each target include time, x, y
-    target_info = {}
-    for target_name in os.listdir("/Users/apple/Desktop/Dataset/GPS"):
-        if os.path.isdir(os.path.join("/Users/apple/Desktop/Dataset/GPS", target_name)):
-            sub_dir = "/Users/apple/Desktop/Dataset/GPS/" + target_name #each target
+    for target_name in os.listdir(folder):
+        if os.path.isdir(os.path.join(folder, target_name)):
+            sub_dir = os.path.join(folder, target_name)
             list_of_csv = glob.glob(sub_dir + "/*.csv")
-            target_info[target_name] = []
+            target_info = []
+            logger.info('Started reading target {}'.format(target_name))
+            #concat all files in one
             for csv_file in list_of_csv:
                 field = ['time', 'lat', 'lon']
                 target_csv_data_file = pandas.read_csv(csv_file, skipinitialspace=True, usecols=field)
-                target_info[target_name].append(target_csv_data_file)
-
-    list = analyze(100, 60, target_info, data_file)
-    print(list)
+                target_info.append(target_csv_data_file)
+            target_file = pandas.concat(target_info)
+            target_file = target_file[target_file.time.notnull()] # remove all NaN time values
+            target_file['time'] =pandas.to_datetime(target_file.time)
+            assert target_file.time.dt.normalize().nunique() == len(list_of_csv)
+            logger.info('Finished reading target {}'.format(target_name))
+            #start analysis for one person
+            logger.info('Started analysis for target {}'.format(target_name))
+            list = analyze(100, 60, target_file, data_file)
+            logger.info('Finished analysis for target {}'.format(target_name))
+            exit()
