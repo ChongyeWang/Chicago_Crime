e3d358fb895faeb601a5f42094a1db89273f23ae
Chongye Wang <wangchongye@126.com>
2018-02-17 00:02:07 -0600

    Update chicago_crime_analysis.py

--- chicago_crime_analysis.py
+++ chicago_crime_analysis.py
@@ -20,7 +20,7 @@ import numpy as np
 import multiprocessing
 #from progressbar import ProgressBar, SimpleProgress
 import tqdm
-
+'''
 def parallelize_dataframe(distance_range, time_range, target_file, data_file, func):
     num_cores = multiprocessing.cpu_count()-1 #leave one free to not freeze machine
     num_partitions = 19000#num_cores #number of partitions to split dataframe
@@ -37,7 +37,7 @@ def parallelize_dataframe(distance_range, time_range, target_file, data_file, fu
     pool.join()
     df = pandas.concat(results)
     return df
-
+'''
 
 def closest(target_file_row, data_file_row, time_range, distance_range):
     try:
@@ -47,37 +47,52 @@ def closest(target_file_row, data_file_row, time_range, distance_range):
     except:
         return False
 
-def analyze(args):
-    target_file, data_file, time_range, distance_range = args
+def closest_distance(target_file_row, data_file_row, distance_range):
+    try:
+        distance = geopy.distance.vincenty((target_file_row.lat, target_file_row.lon), (data_file_row.lat, data_file_row.lon)).m
+        return distance <= distance_range
+    except:
+        return False
+
+def analyze(distance_range, time_range, target_file, data_file):
+    #target_file, data_file, time_range, distance_range = args
     satisfied_target = []
+    curr_df_date = data_file['time'][0] #current earliest date of data file
     for target_index, target_row in target_file.iterrows():
         #print('Checking target row {}'.format(target_index))
         #logger.info('Checking target row {}'.format(target_index))
-        for data_index, data_row in data_file.iterrows():
-            """if(data_index%10000==0):
-                #logger.info('Checking data row {}'.format(data_index))
-                print('Checking data row {}'.format(data_index))"""
-        if(closest(target_row, data_row, time_range, distance_range)):
-                #time_t, y_t, x_t, x_d, y_d, time_t, type_d
+        curr_target_date = target_row.time
+        lower_bound_date = curr_target_date - timedelta(seconds=time_range)
+        upper_bound_date = curr_target_date + timedelta(seconds=time_range)
+        find_first = False
+        for row_index, data_row in data_file[data_file['time'] >= curr_df_date].iterrows():
+            curr_row_date = data_row.time
+            if curr_df_date < lower_bound_date: continue
+            if curr_df_date > upper_bound_date: break
+            if find_first == False:
+                curr_df_date = curr_row_date
+                find_first = True
+            if closest_distance(target_row, data_row, distance_range):
                 info_list = target_row.tolist()
                 info_list.extend(data_row.tolist())
                 satisfied_target.append(info_list)
-        #logger.info('Finished target row {}'.format(target_index))
+
     return satisfied_target
 
 
 if __name__ == "__main__":
     #Columns that are included in data_file
     fields = ['X', 'Y', 'USER_Event_Type', 'USER_Entry_Date___Time'] #changed the date field to the one that looks correct need to ask
-    folder = 'Dataset/GPS/'
+    folder = '/Users/apple/Desktop/Dataset/GPS/'
     filename = 'FA171521_ARCGIS_GPS_50.csv'
     #Include the x, y, user_event_type, user_clrdate
     logger.info('Started reading data file')
     data_file = pandas.read_csv(os.path.join(folder, filename),skipinitialspace=True, usecols=fields)
     data_file = data_file.rename(columns={'USER_Entry_Date___Time': 'time', 'X':'lon', 'Y':'lat'}) #lets keep all times times
-    data_file = data_file[data_file.time.notnull()] # remove all NaN time values
+    #data_file = data_file[data_file.time.notnull()] # remove all NaN time values
     data_file['time'] = pandas.to_datetime(data_file.time)
     logger.info('Finished reading data file')
+    data_file = data_file.sort_values(by=['time'], ascending=[True])
 
     #Each target include time, x, y
     for target_name in os.listdir(folder):
@@ -95,11 +110,21 @@ if __name__ == "__main__":
             target_file = target_file[target_file.time.notnull()] # remove all NaN time values
             target_file['time'] =pandas.to_datetime(target_file.time)
             assert target_file.time.dt.normalize().nunique() == len(list_of_csv)
+
             logger.info('Finished reading target {}'.format(target_name))
             #start analysis for one person
             logger.info('Started analysis for target {}'.format(target_name))
             #list = analyze(100, 60, target_file, data_file)
-            list = parallelize_dataframe(100, 60, target_file, data_file, analyze)
+
+            target_file = target_file.sort_values(by=['time'], ascending=[True])
+
+            #list = parallelize_dataframe(100, 60, target_file, data_file, analyze)
+            list = analyze(100, 60, target_file, data_file)
             logger.info('Finished analysis for target {}'.format(target_name))
-            list.to_csv(file_name, sep=',', encoding='utf-8')
+
+            #list.to_csv(str(target_name) + '.csv', sep=',', encoding='utf-8')
+            with open(str(target_name) + '.csv', 'w', newline='') as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerows(list)
+
             exit()
