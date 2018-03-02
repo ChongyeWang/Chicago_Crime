e0ab2ed4f0e7ed8f062630e54776916f840d7644
Chongye Wang <wangchongye@126.com>
2018-03-02 00:51:58 -0600

    Update chicago_crime_analysis.py

--- chicago_crime_analysis.py
+++ chicago_crime_analysis.py
@@ -20,6 +20,7 @@ import numpy as np
 import multiprocessing
 import tqdm
 import argparse
+import time
 
 #ps -u | grep chicago_crime_analysis.py | awk '{print $2}' | xargs kill -KILL #kills all parallel processes
 
@@ -31,63 +32,98 @@ def parallelize_dataframe(distance_range, time_range, target_file, data_file, wh
     num_cores = multiprocessing.cpu_count()-1 #leave one free to not freeze machine
     #num_partitions = 10#num_cores #number of partitions to split dataframe
     df_split = np.array_split(split_file, num_partitions)
-    print len(df_split)
+    #print len(df_split)
     a = [(split, data_file, time_range, distance_range) for split in df_split]
-    print len(a)
+    #print len(a)
     pool = multiprocessing.Pool(num_cores)
     results = []
     for x in tqdm.tqdm(pool.imap_unordered(func, a), total=num_partitions):
         results.extend(x)
     pool.close()
     pool.join()
-    print results
+    #print results
     return results
 
 
-def closest(target_file_row, data_file_row, time_range, distance_range):
-    try:
-        distance = geopy.distance.vincenty((target_file_row.lat, target_file_row.lon), (data_file_row.lat, data_file_row.lon)).m
-        delta = abs(data_file_row.time - target_file_row.time)
-        return delta <= timedelta(seconds=time_range) and distance <= distance_range
-    except:
-        return False
+#def closest(target_file_row, data_file_row, time_range, distance_range):
+#    try:
+#        distance = geopy.distance.vincenty((target_file_row.lat, target_file_row.lon), (data_file_row.lat, data_file_row.lon)).m
+#        delta = abs(data_file_row.time - target_file_row.time)
+#        return delta <= timedelta(seconds=time_range) and distance <= distance_range
+#    except:
+#        return False
 
 def closest_distance(target_file_row, data_file_row, distance_range):
     try:
         distance = geopy.distance.vincenty((target_file_row.lat, target_file_row.lon), (data_file_row.lat, data_file_row.lon)).m
-        return distance <= distance_range
+        #return distance <= distance_range
+        if distance > distance_range:
+            return -1
+        return distance
     except:
-        return False
+        return -1
 
 def analyze(args):#target_file, data_file, time_range, distance_range):
     target_file, data_file, time_range, distance_range = args
     satisfied_target = []
     curr_df_date = data_file['time'][0] #[13000] #current earliest date of data file
-    for target_index, target_row in tqdm.tqdm(target_file.iterrows(), total=len(target_file)):
+    nCount = 0
+    nCrime = 0
+    row_start = 0
+    row_offset = 0
+    
+    for target_index, target_row in target_file.iterrows():
         #print('Checking target row {}'.format(target_index))
         #logger.info('Checking target row {}'.format(target_index))
+
+        #print("");
         curr_target_date = target_row.time
         lower_bound_date = curr_target_date - timedelta(seconds=time_range)
         upper_bound_date = curr_target_date + timedelta(seconds=time_range)
         find_first = False
-        for row_index, data_row in data_file[data_file['time'] >= curr_df_date].iterrows():
+        nCount = 0
+
+        row_offset = 0
+        #print("Searching for: ", curr_target_date)
+        #print("Range: ", lower_bound_date, " - ", upper_bound_date);
+        for row_index in range(row_start, len(data_file)): #data_row in data_file[data_file['time'] >= curr_df_date].iterrows():
+            data_row = data_file.iloc[row_index]
             curr_row_date = data_row.time
-            if curr_df_date < lower_bound_date: continue
-            if curr_df_date > upper_bound_date: break
-            if find_first == False:
-                curr_df_date = curr_row_date
-                find_first = True
-            if closest_distance(target_row, data_row, distance_range):
+
+            row_offset = row_offset + 1
+            nCount = nCount + 1
+
+            if curr_row_date < lower_bound_date: continue
+            if curr_row_date > upper_bound_date:
+                 break
+            # curr_row_date >= low && curr_row_date <= high
+            #if find_first == False:
+            #    curr_df_date = curr_row_date
+            #    find_first = True
+            #print(curr_row_date)
+            distance = closest_distance(target_row, data_row, distance_range)
+            if (distance != -1):
                 info_list = target_row.tolist()
                 info_list.extend(data_row.tolist())
+                info_list.extend([distance])
                 satisfied_target.append(info_list)
-    print "satisfied_target",satisfied_target
+                nCrime = nCrime + 1
+                
+        #Subtract 1 to ensure next loop doesn't skip a row
+        row_start = row_start + row_offset - 1
+         
+        #print("Next row start: ", row_start, "(", row_index, ")")
+        #print("Next crime start: ", curr_row_date, "(", data_file.iloc[row_start].time, ")")
+        #print("Comparisons: ", nCount)
+    print("Crime exposures: ", nCrime)
+    print ("satisfied_target",satisfied_target)
     return satisfied_target
 
 
 def run_one_folder(folder, target_name, args):
     sub_dir = os.path.join(folder, target_name)
     list_of_csv = glob.glob(sub_dir + "/*.csv")
+    print("Mobile files detected: ", len(list_of_csv))
     target_info = []
     logger.info('Started reading target {}'.format(target_name))
     #concat all files in one
@@ -103,15 +139,20 @@ def run_one_folder(folder, target_name, args):
 
     #assert target_file.time.dt.normalize().nunique() == len(list_of_csv)
     logger.info('Finished reading target {}'.format(target_name))
+
+    print("Mobile GPS samples: ", len(target_file.index))
+
     #start analysis for one person
     logger.info('Started analysis for target {}'.format(target_name))
 
     target_file = target_file.sort_values(by=['time'], ascending=[True])
 
     if(args.parallelize):
-        list = parallelize_dataframe(100, 60, target_file, data_file, args.which, args.npartitions, analyze)
+        print("Run analysis in parralel")
+        list = parallelize_dataframe(args.distance, 60, target_file, data_file, args.which, args.npartitions, analyze)
     else:
-        arguments = target_file, data_file, 60, 100
+        print("Run analysis sequentially")
+        arguments = target_file, data_file, 60, args.distance
         list = analyze(arguments)
     logger.info('Finished analysis for target {}'.format(target_name))
 
@@ -119,15 +160,26 @@ def run_one_folder(folder, target_name, args):
 
 if __name__ == "__main__":
 
+    subject = '3'
+    distance = 100
+    #crimedata = 'Incident Reports.csv' 
+    #analysis_type = 'IR'
+
+    crimedata = 'FA171521_ARCGIS_GPS_50_V1_2.csv'
+    analysis_type = '911'
+
+    print ("Program start for: ", subject)
     parser = argparse.ArgumentParser(description="train.py")
     parser.add_argument("-folder", default='Dataset/GPS/', help="Path to the *-train.pt file from preprocess.py")
-    parser.add_argument("-filename", default='FA171521_ARCGIS_GPS_50_V1_2.csv', help="Filename for police file")
-    parser.add_argument("-tgt_folder", default=None, help="Target folder")
+    parser.add_argument("-filename", default=crimedata, help="Filename for police file")
+    parser.add_argument("-tgt_folder", default=subject, help="Target folder")
     parser.add_argument("-parallelize", default=0, type=int, help="run in parallel:1, no:0")
     parser.add_argument("-which", default=1, type=int, help="which file to split for parallel. 1:fitbit file, otherwise fitbit file")
     parser.add_argument("-npartitions", default=10, type=int, help="number of partitions for parallel, needs to be smaller than number of file rows")
+    parser.add_argument("-distance", default=distance)
     args = parser.parse_args()
 
+    starttime = time.time()
     #Columns that are included in data_file
     fields = ['X', 'Y', 'USER_Event_Type', 'USER_Entry_Date___Time'] #changed the date field to the one that looks correct need to ask
     folder = args.folder
@@ -139,15 +191,22 @@ if __name__ == "__main__":
     #data_file = data_file[data_file.time.notnull()] # remove all NaN time values
     data_file['time'] = pandas.to_datetime(data_file.time)
     logger.info('Finished reading data file')
+    print("Loading time: ", round(time.time()-starttime,2))
 
     #sort data file
     data_file = data_file.sort_values(by=['time'], ascending=[True])
 
+    print ("Crimes loaded:", len(data_file.index))
+
     if(args.tgt_folder):
+        #check subject data exists
         if os.path.isdir(os.path.join(folder, args.tgt_folder)):
+            #run comparison
             list =  run_one_folder(folder, args.tgt_folder, args)
+            #output results
             df =  pandas.DataFrame(list)
-            df.to_csv(str(args.tgt_folder) + '.csv', sep=',', encoding='utf-8')
+            df.to_csv(str(args.tgt_folder) + ' ' + analysis_type + '.csv', sep=',', encoding='utf-8')
+            
     else:
         #Each target include time, x, y
         for target_name in os.listdir(folder):
